34eb514763b19829680446191539671dc33d67d0
Aidanhubley <ah1632@messiah.edu>
2023-10-11 16:29:01 -0400

    path fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+// const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
-  },
-  plugins: [],
-}
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
 
+      md: "740px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px"
+      // => @media (min-width: 1536px) { ... }
+    },
+    colors: {
+      // ...colors,
+      // bone: "#D7D9CE",
+      // lightgreen: "#6F8C75",
+      // midgreen: "#4A5E4E",
+      // darkgreen: "#062600"
+    }
+  },
+};
